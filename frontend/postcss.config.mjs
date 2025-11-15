138d77bfb16543f1f4cb8d397df8907e1c8b6c60
Roman Steiner <roman@toastlab.ch>
2025-11-15 13:37:10 +0100

    feat: add asset hashing + template specific css/ts

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -6,7 +6,12 @@ const plugins = {
 if (process.env.NODE_ENV === 'production') {
     plugins['cssnano'] = {
         preset: 'default',
-    }
+    };
+    
+    plugins['postcss-hash'] = {
+        algorithm: 'sha256',
+        trim: 12,
+    };
 }
 
 export default {
--- frontend/www/site/snippets/header.php
+++ frontend/www/site/snippets/header.php
@@ -1,10 +1,31 @@
+<?php 
+
+/*
+Preload font example:
+$preload[] = [
+    'url' => url('assets/fonts/Montserrat-latin.woff2'),
+    'type' => 'font',
+    'crossorigin' => true,
+];
+*/
+
+?>
 <!DOCTYPE html>
 <html lang="it-CH">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page->title() ?> - <?= $site->title() ?></title>
-    <?= css('assets/css/main.css') ?>
-    <?= js('assets/js/main.js', ['type' => 'module']) ?>
+    <?php if (isset($preload) && is_array($preload)): ?>
+        <?php foreach ($preload as $item): ?>
+            <?php if (isset($item['url']) && isset($item['type'])): ?>
+                <link rel="preload" <?= attr(['href' => $item['url'], 'as' => $item['type'], 'crossorigin' => $item['crossorigin'] ?? null]) ?>>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    <?php endif; ?>
+    <?= css(['assets/css/main.css']) ?>
+    <?= cssTpl() ?>
+    <?= js(['assets/js/main.js'], ['type' => 'module']) ?>
+    <?= jsTpl(['type' => 'module']) ?>
 </head>
 <body>
